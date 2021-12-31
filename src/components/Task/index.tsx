5d79ae8debc18267b83e426e3ad17f56c9f4c47b
Flavio Santos <flvsantos300@gmail.com>
2021-12-31 09:31:50 -0300

    new features

---
+++ src/components/Task/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+} from 'react-native'
+
+export function Task(){
+  return(
+    <View>
+      {/* nome da task */}
+      {/* contador da task */}
+      {/* botao de inicio */}
+      {/* botao para finalizar */}
+      {/* com uma funçao que ao ser finalizado vai voltar a tela inicial*/}
+    </View>
+  )
+}
+
+const stytes = StyleSheet.create({
+  
+})
